9e720199a0759786578e420111023a1647a5eac2
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2023-02-24 16:21:56 +0100

    Flock adjusted

--- Assets/Flock/Scripts/BehaviorScripts/StayInRadiusBehavior.cs
+++ Assets/Flock/Scripts/BehaviorScripts/StayInRadiusBehavior.cs
@@ -11,9 +11,9 @@ public class StayInRadiusBehavior : FlockBehavior
     public override Vector3 CalculateMove(Boid boid, List<Transform> context, Flock flock)
     {
         Vector3 centerOffset = center - boid.transform.position;
-        Debug.Log("centerOffset: " + centerOffset);
+        //Debug.Log("centerOffset: " + centerOffset);
         float t = centerOffset.magnitude / radius;
-        Debug.Log("t: " + t);
+        //Debug.Log("t: " + t);
         if (t < 0.9f)
         {
             return Vector3.zero;
